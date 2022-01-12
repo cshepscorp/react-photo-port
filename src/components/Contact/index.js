6fd9f607cdc473c65adbde3aef9b4c75f48a8322
cshepscorp <sheppard.christy@gmail.com>
2022-01-12 10:50:21 -0500

    contact form validating

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-// import PhotoList from '../PhotoList';
-// import { capitalizeFirstLetter } from '../../utils/helpers';
+import { validateEmail } from '../../utils/helpers';
 
 
 /* Notice that we named the function ContactForm, not Contact. The name of this function isn't critical and doesn't have to match the name of the component folder. But the name of the function needs to be in the export statement. */
@@ -9,10 +8,32 @@ function ContactForm() {
     // set the initial state to empty strings
     // three key-value pairs to represent the three user inputs: name, email, and message
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    
+    const [errorMessage, setErrorMessage] = useState(''); // initial state is an empty string; assign the value of errorMessage based on isValid value
     const { name, email, message } = formState;
     // sync the internal state of the component formState with the user input from the DOM. The onChange event listener will ensure that the handleChange function fires whenever a keystroke is typed into the input field for name; use the spread operator, ...formState, so we can retain the other key-value pairs in this object; without the spread op, the other values in formState would be overwritten, leaving only the name: value key pair
     function handleChange(e) {
-      setFormState({...formState, [e.target.name]: e.target.value }) // The name property of target in the preceding expression actually refers to the name attribute of the form element. This attribute value matches the property names of formState (name, email, and message) and allows us to use [ ] to create dynamic property names.
+      if (e.target.name === 'email') {
+        const isValid = validateEmail(e.target.value);
+        console.log('isValid? ' + isValid);
+        // isValid conditional statement
+        if (!isValid) {
+          setErrorMessage('Your email is invalid.');
+        } else {
+          setErrorMessage('');
+        }
+      } else {
+        if (!e.target.value.length) {
+          setErrorMessage(`${e.target.name} is required.`);
+        } else {
+          setErrorMessage('');
+        }
+      }
+      console.log('errorMessage', errorMessage);
+      if (!errorMessage) {
+        setFormState({...formState, [e.target.name]: e.target.value }) // The name property of target in the preceding expression actually refers to the name attribute of the form element. This attribute value matches the property names of formState (name, email, and message) and allows us to use [ ] to create dynamic property names.
+      }
+      
     }
     function handleSubmit(e) {
       e.preventDefault();
@@ -26,16 +47,21 @@ function ContactForm() {
                 <div>
                     <label htmlFor="name">Name:</label>
                     {/* add the attribute onChange to the <input> element for name, and assign a function to this attribute, aptly called handleChange */}
-                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+                    <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+                    <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
+                    <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5"  />
                 </div>
+                {errorMessage && (
+                  <div>
+                    <p className="error-text">{errorMessage}</p>
+                  </div>
+                )}
                 <button type="submit">Submit</button>
           </form>
         </section>
